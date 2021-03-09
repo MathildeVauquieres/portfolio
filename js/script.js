47cb4f69a17d3b27d73b0aa48fb2b8e333970730
Mathilde <vauquieres.m@laposte.net>
2021-03-09 15:56:49 +0100

    rangement feuilles css/html/js

--- js/script.js
+++ js/script.js
@@ -1,174 +1,142 @@
-
 window.onload = () => {
-   window.addEventListener("scroll", () => {
-       let hauteur = document.documentElement.scrollHeight - window.innerHeight
-
-       let position = window.scrollY
-
-       let largeur = document.documentElement.clientWidth
-
-       let barre = position / hauteur * largeur
-
-       document.getElementById("progress").style.width = barre+"px"
-   })
+	window.addEventListener("scroll", () => {
+		let hauteur = document.documentElement.scrollHeight - window.innerHeight
+		let position = window.scrollY
+		let largeur = document.documentElement.clientWidth
+		let barre = position / hauteur * largeur
+		document.getElementById("progress").style.width = barre + "px"
+	})
 }
-
- $(document).ready(function() {
-
-   $(window).on("load scroll", function() {
-     var parallaxElement = $(".parallax_scroll"),
-       parallaxQuantity = parallaxElement.length;
-     window.requestAnimationFrame(function() {
-       for (var i = 0; i < parallaxQuantity; i++) {
-         var currentElement = parallaxElement.eq(i),
-           windowTop = $(window).scrollTop(),
-           elementTop = currentElement.offset().top,
-           elementHeight = currentElement.height(),
-           viewPortHeight = window.innerHeight * 0.5 - elementHeight * 0.5,
-           scrolled = windowTop - elementTop + viewPortHeight;
-         currentElement.css({
-           transform: "translate3d(0," + scrolled * 0.1 + "px, 0)"
-         });
-       }
-     });
-   });
- });
-
- $(document).ready(function() {
-
-   $(window).on("load scroll", function() {
-     var parallaxElement = $(".parallax_scroll1"),
-       parallaxQuantity = parallaxElement.length;
-     window.requestAnimationFrame(function() {
-       for (var i = 0; i < parallaxQuantity; i++) {
-         var currentElement = parallaxElement.eq(i),
-           windowTop = $(window).scrollTop(),
-           elementTop = currentElement.offset().top,
-           elementHeight = currentElement.height(),
-           viewPortHeight = window.innerHeight * 0.5 - elementHeight * 0.5,
-           scrolled = windowTop - elementTop + viewPortHeight;
-         currentElement.css({
-           transform: "translate3d(0," + scrolled * -0.1 + "px, 0)"
-         });
-       }
-     });
-   });
- });
-
- 
- $(document).ready(function() {
-
-  $(window).on("load scroll", function() {
-    var parallaxElement = $("#color1"),
-      parallaxQuantity = parallaxElement.length;
-    window.requestAnimationFrame(function() {
-      for (var i = 0; i < parallaxQuantity; i++) {
-        var currentElement = parallaxElement.eq(i),
-          windowTop = $(window).scrollTop(),
-          elementTop = currentElement.offset().top,
-          elementHeight = currentElement.height(),
-          viewPortHeight = window.innerHeight * 0.5 - elementHeight * 0.5,
-          scrolled = windowTop - elementTop + viewPortHeight;
-        currentElement.css({
-          transform: "translate3d(0," + scrolled * 0.05 + "px, 0)"
-        });
-      }
-    });
-  });
-});
-
 $(document).ready(function() {
-
-  $(window).on("load scroll", function() {
-    var parallaxElement = $("#color2"),
-      parallaxQuantity = parallaxElement.length;
-    window.requestAnimationFrame(function() {
-      for (var i = 0; i < parallaxQuantity; i++) {
-        var currentElement = parallaxElement.eq(i),
-          windowTop = $(window).scrollTop(),
-          elementTop = currentElement.offset().top,
-          elementHeight = currentElement.height(),
-          viewPortHeight = window.innerHeight * 0.5 - elementHeight * 0.5,
-          scrolled = windowTop - elementTop + viewPortHeight;
-        currentElement.css({
-          transform: "translate3d(0," + scrolled * -0.05 + "px, 0)"
-        });
-      }
-    });
-  });
+	$(window).on("load scroll", function() {
+		var parallaxElement = $(".parallax_scroll"),
+			parallaxQuantity = parallaxElement.length;
+		window.requestAnimationFrame(function() {
+			for (var i = 0; i < parallaxQuantity; i++) {
+				var currentElement = parallaxElement.eq(i),
+					windowTop = $(window).scrollTop(),
+					elementTop = currentElement.offset().top,
+					elementHeight = currentElement.height(),
+					viewPortHeight = window.innerHeight * 0.5 - elementHeight * 0.5,
+					scrolled = windowTop - elementTop + viewPortHeight;
+				currentElement.css({
+					transform: "translate3d(0," + scrolled * 0.1 + "px, 0)"
+				});
+			}
+		});
+	});
 });
- 
 $(document).ready(function() {
-
-  $(window).on("load scroll", function() {
-    var parallaxElement = $("#color3"),
-      parallaxQuantity = parallaxElement.length;
-    window.requestAnimationFrame(function() {
-      for (var i = 0; i < parallaxQuantity; i++) {
-        var currentElement = parallaxElement.eq(i),
-          windowTop = $(window).scrollTop(),
-          elementTop = currentElement.offset().top,
-          elementHeight = currentElement.height(),
-          viewPortHeight = window.innerHeight * 0.5 - elementHeight * 0.5,
-          scrolled = windowTop - elementTop + viewPortHeight;
-        currentElement.css({
-          transform: "translate3d(0," + scrolled * 0.05 + "px, 0)"
-        });
-      }
-    });
-  });
+	$(window).on("load scroll", function() {
+		var parallaxElement = $(".parallax_scroll1"),
+			parallaxQuantity = parallaxElement.length;
+		window.requestAnimationFrame(function() {
+			for (var i = 0; i < parallaxQuantity; i++) {
+				var currentElement = parallaxElement.eq(i),
+					windowTop = $(window).scrollTop(),
+					elementTop = currentElement.offset().top,
+					elementHeight = currentElement.height(),
+					viewPortHeight = window.innerHeight * 0.5 - elementHeight * 0.5,
+					scrolled = windowTop - elementTop + viewPortHeight;
+				currentElement.css({
+					transform: "translate3d(0," + scrolled * -0.1 + "px, 0)"
+				});
+			}
+		});
+	});
 });
-
 $(document).ready(function() {
-
-  $(window).on("load scroll", function() {
-    var parallaxElement = $(".cc1b"),
-      parallaxQuantity = parallaxElement.length;
-    window.requestAnimationFrame(function() {
-      for (var i = 0; i < parallaxQuantity; i++) {
-        var currentElement = parallaxElement.eq(i),
-          windowTop = $(window).scrollTop(),
-          elementTop = currentElement.offset().top,
-          elementHeight = currentElement.height(),
-          viewPortHeight = window.innerHeight * 0.5 - elementHeight * 0.5,
-          scrolled = windowTop - elementTop + viewPortHeight;
-        currentElement.css({
-          transform: "translate3d(0," + scrolled * 0.2 + "px, 0)"
-        });
-      }
-    });
-  });
+	$(window).on("load scroll", function() {
+		var parallaxElement = $("#color1"),
+			parallaxQuantity = parallaxElement.length;
+		window.requestAnimationFrame(function() {
+			for (var i = 0; i < parallaxQuantity; i++) {
+				var currentElement = parallaxElement.eq(i),
+					windowTop = $(window).scrollTop(),
+					elementTop = currentElement.offset().top,
+					elementHeight = currentElement.height(),
+					viewPortHeight = window.innerHeight * 0.5 - elementHeight * 0.5,
+					scrolled = windowTop - elementTop + viewPortHeight;
+				currentElement.css({
+					transform: "translate3d(0," + scrolled * 0.05 + "px, 0)"
+				});
+			}
+		});
+	});
 });
-
 $(document).ready(function() {
-
-  $(window).on("load scroll", function() {
-    var parallaxElement = $(".cc1bb"),
-      parallaxQuantity = parallaxElement.length;
-    window.requestAnimationFrame(function() {
-      for (var i = 0; i < parallaxQuantity; i++) {
-        var currentElement = parallaxElement.eq(i),
-          windowTop = $(window).scrollTop(),
-          elementTop = currentElement.offset().top,
-          elementHeight = currentElement.height(),
-          viewPortHeight = window.innerHeight * 0.5 - elementHeight * 0.5,
-          scrolled = windowTop - elementTop + viewPortHeight;
-        currentElement.css({
-          transform: "translate3d(0," + scrolled * -0.1 + "px, 0)"
-        });
-      }
-    });
-  });
+	$(window).on("load scroll", function() {
+		var parallaxElement = $("#color2"),
+			parallaxQuantity = parallaxElement.length;
+		window.requestAnimationFrame(function() {
+			for (var i = 0; i < parallaxQuantity; i++) {
+				var currentElement = parallaxElement.eq(i),
+					windowTop = $(window).scrollTop(),
+					elementTop = currentElement.offset().top,
+					elementHeight = currentElement.height(),
+					viewPortHeight = window.innerHeight * 0.5 - elementHeight * 0.5,
+					scrolled = windowTop - elementTop + viewPortHeight;
+				currentElement.css({
+					transform: "translate3d(0," + scrolled * -0.05 + "px, 0)"
+				});
+			}
+		});
+	});
 });
-
-
-
-
-
-
- 
-
-
-
-
-
+$(document).ready(function() {
+	$(window).on("load scroll", function() {
+		var parallaxElement = $("#color3"),
+			parallaxQuantity = parallaxElement.length;
+		window.requestAnimationFrame(function() {
+			for (var i = 0; i < parallaxQuantity; i++) {
+				var currentElement = parallaxElement.eq(i),
+					windowTop = $(window).scrollTop(),
+					elementTop = currentElement.offset().top,
+					elementHeight = currentElement.height(),
+					viewPortHeight = window.innerHeight * 0.5 - elementHeight * 0.5,
+					scrolled = windowTop - elementTop + viewPortHeight;
+				currentElement.css({
+					transform: "translate3d(0," + scrolled * 0.05 + "px, 0)"
+				});
+			}
+		});
+	});
+});
+$(document).ready(function() {
+	$(window).on("load scroll", function() {
+		var parallaxElement = $(".cc1b"),
+			parallaxQuantity = parallaxElement.length;
+		window.requestAnimationFrame(function() {
+			for (var i = 0; i < parallaxQuantity; i++) {
+				var currentElement = parallaxElement.eq(i),
+					windowTop = $(window).scrollTop(),
+					elementTop = currentElement.offset().top,
+					elementHeight = currentElement.height(),
+					viewPortHeight = window.innerHeight * 0.5 - elementHeight * 0.5,
+					scrolled = windowTop - elementTop + viewPortHeight;
+				currentElement.css({
+					transform: "translate3d(0," + scrolled * 0.2 + "px, 0)"
+				});
+			}
+		});
+	});
+});
+$(document).ready(function() {
+	$(window).on("load scroll", function() {
+		var parallaxElement = $(".cc1bb"),
+			parallaxQuantity = parallaxElement.length;
+		window.requestAnimationFrame(function() {
+			for (var i = 0; i < parallaxQuantity; i++) {
+				var currentElement = parallaxElement.eq(i),
+					windowTop = $(window).scrollTop(),
+					elementTop = currentElement.offset().top,
+					elementHeight = currentElement.height(),
+					viewPortHeight = window.innerHeight * 0.5 - elementHeight * 0.5,
+					scrolled = windowTop - elementTop + viewPortHeight;
+				currentElement.css({
+					transform: "translate3d(0," + scrolled * -0.1 + "px, 0)"
+				});
+			}
+		});
+	});
+});
